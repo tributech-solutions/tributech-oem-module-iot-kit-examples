7c20720ff4894028e55284d843a6b09da10cdf7e
Simon Pfeifhofer <s.pfeifhofer@tributech.io>
2022-05-20 16:10:34 +0200

    feat: WIP merge

--- nRF52840-DK/DPS368_to_OEM/src/UartCommunication/uart_communication.c
+++ nRF52840-DK/DPS368_to_OEM/src/UartCommunication/uart_communication.c
@@ -11,10 +11,15 @@
 #include <parse_and_send.h>
 
 const struct device *uart;
+const struct device *usb;
+
 struct k_sem uart_sem;
 
 static K_MEM_SLAB_DEFINE(uart_slab, BUF_SIZE, 2, 4);
-uint8_t *buf;
+static K_MEM_SLAB_DEFINE(usb_slab, BUF_SIZE, 2, 4);
+
+uint8_t *buf_oem;
+uint8_t *buf_usb;
 
 LOG_MODULE_REGISTER(uart_communication,LOG_LEVEL_INF);
 
@@ -31,16 +36,16 @@ void uart_callback(const struct device *dev,
     switch (evt->type)
     {
         case UART_TX_DONE:
-                LOG_INF("UART_TX_DONE");
+                LOG_INF("OEM UART_TX_DONE");
                 break;
 
         case UART_TX_ABORTED:
-                LOG_INF("UART_TX_ABORTED");
+                LOG_INF("OEM UART_TX_ABORTED");
                 break;
 
         case UART_RX_RDY:
                 // Save it to uart receive buffer
-                LOG_INF("UART_RX_RDY");
+                LOG_INF("OEM UART_RX_RDY");
                 uart_rx_disable(uart);
 
                 while(first_bracket_offset < 4096 && evt->data.rx.buf[first_bracket_offset] != 0x7B)
@@ -62,28 +67,73 @@ void uart_callback(const struct device *dev,
 
         case UART_RX_BUF_REQUEST:
         {
-            LOG_INF("UART_RX_BUF_REQUEST");
-            uint8_t *buf;
+            LOG_INF("OEM UART_RX_BUF_REQUEST");
+            uint8_t *buf_oem;
 
-            err = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
+            err = k_mem_slab_alloc(&uart_slab, (void **)&buf_oem, K_NO_WAIT);
             __ASSERT(err == 0, "Failed to allocate slab");
 
-            err = uart_rx_buf_rsp(uart, buf, BUF_SIZE);
-            __ASSERT(err == 0, "Failed to provide new buffer");
+            err = k_mem_slab_alloc(&usb_slab, (void **)&buf_usb, K_NO_WAIT);
+            __ASSERT(err == 0, "Failed to allocate slab");
+
+            err = uart_rx_buf_rsp(uart, buf_oem, BUF_SIZE);
+            __ASSERT(err == 0, "Failed to provide new buffer for OEM communication");
+
+            err = uart_rx_buf_rsp(usb, buf_usb, BUF_SIZE);
+            __ASSERT(err == 0, "Failed to provide new buffer for USB communication");
             break;
         }
 
         case UART_RX_BUF_RELEASED:
-            LOG_INF("UART_RX_BUF_RELEASED");
+            LOG_INF("OEM UART_RX_BUF_RELEASED");
             k_mem_slab_free(&uart_slab, (void **)&evt->data.rx_buf.buf);
             break;
 
         case UART_RX_DISABLED:
-            LOG_INF("UART_RX_DISABLED");
+            LOG_INF("OEM UART_RX_DISABLED");
             break;
 
         case UART_RX_STOPPED:
-            LOG_INF("UART_RX_STOPPED");
+            LOG_INF("OEM UART_RX_STOPPED");
+            break;
+    }
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// usb callback
+void usb_callback(const struct device *dev,
+                   struct uart_event *evt,
+                   void *user_data)
+{
+
+    switch (evt->type)
+    {
+        case UART_TX_DONE:
+            LOG_INF("USB UART_TX_DONE");
+            break;
+
+        case UART_TX_ABORTED:
+            LOG_INF("USB UART_TX_ABORTED");
+            break;
+
+        case UART_RX_RDY:
+            LOG_INF("USB UART_RX_RDY");
+            break;
+
+        case UART_RX_BUF_REQUEST:
+            LOG_INF("USB UART_RX_BUF_REQUEST");
+            break;
+
+        case UART_RX_BUF_RELEASED:
+            LOG_INF("USB UART_RX_BUF_RELEASED");
+            break;
+
+        case UART_RX_DISABLED:
+            LOG_INF("USB UART_RX_DISABLED");
+            break;
+
+        case UART_RX_STOPPED:
+            LOG_INF("USB UART_RX_STOPPED");
             break;
     }
 }
@@ -95,16 +145,28 @@ void uart_init(void)
     int err = 0;
 
     uart = device_get_binding("UART_1");
-    __ASSERT(uart, "Failed to get the device");
+    __ASSERT(uart, "Failed to get the UART_1 device");
 
-    err = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
+    usb = device_get_binding("CDC_ACM_0");
+    __ASSERT(usb == 0, "Failed to get the CDC_ACM_0 (USB) device");
+
+    err = k_mem_slab_alloc(&uart_slab, (void **)&buf_oem, K_NO_WAIT);
+    __ASSERT(err == 0, "Failed to alloc slab");
+
+    err = k_mem_slab_alloc(&usb_slab, (void **)&buf_usb, K_NO_WAIT);
     __ASSERT(err == 0, "Failed to alloc slab");
 
     err = uart_callback_set(uart, uart_callback, (void *)uart);   // set callback
-    __ASSERT(err == 0, "Failed to set callback");
+    __ASSERT(err == 0, "Failed to set OEM callback");
 
-    err = uart_rx_enable(uart, buf, BUF_SIZE, 100);    // enable receiving
-    __ASSERT(err == 0, "Failed to enable RX"); 
+    err = uart_callback_set(usb, usb_callback, (void *)usb);   // set callback
+    __ASSERT(err == 0, "Failed to set USB callback");
+
+    err = uart_rx_enable(uart, buf_oem, BUF_SIZE, 100);    // enable receiving
+    __ASSERT(err == 0, "Failed to enable RX OEM"); 
+
+    err = uart_rx_enable(usb, buf_usb, BUF_SIZE, 100);    // enable receiving
+    __ASSERT(err == 0, "Failed to enable RX USB"); 
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -113,7 +175,7 @@ void uart_reinit(void)
 {
     int err = 0;
 
-    err = uart_rx_enable(uart, buf, BUF_SIZE, 100);    // enable receiving
+    err = uart_rx_enable(uart, buf_oem, BUF_SIZE, 100);    // enable receiving
     __ASSERT(err == 0, "Failed to enable RX");
 
     k_sleep(K_MSEC(10)); 
